bf24ff2ce4aef7a591c2e77f656ab0d8802a8975
navetagrover <navetagrover.ak@gmail.com>
2024-02-28 14:59:08 +0530

    deployment startegies with probes

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+// Define a route for the /healthz endpoint
+app.get('/healthz', (req, res) => {
+  // Respond with a simple JSON object indicating that the application is healthy
+  res.json({ status: 'ok' });
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
