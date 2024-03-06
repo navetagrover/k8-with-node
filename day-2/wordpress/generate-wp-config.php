44b87e19b9953fb533328194cc1e9e65e570eabd
navetagrover <navetagrover.ak@gmail.com>
2024-03-06 14:46:46 +0530

    day2 wordpress using k8

---
+++ day-2/wordpress/generate-wp-config.php
@@ -0,0 +1,26 @@
+<?php
+// WordPress Database Settings
+define( 'DB_NAME', getenv('MYSQL_DATABASE') );
+define( 'DB_USER', getenv('MYSQL_USER') );
+define( 'DB_PASSWORD', getenv('MYSQL_PASSWORD') );
+define( 'DB_HOST', getenv('MYSQL_HOST') );
+define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_COLLATE', '' );
+
+// Authentication Unique Keys and Salts
+// Replace these unique phrases with your own from the WordPress Salt Generator.
+define( 'AUTH_KEY',         getenv('AUTH_KEY') );
+define( 'SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY') );
+define( 'LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY') );
+define( 'NONCE_KEY',        getenv('NONCE_KEY') );
+define( 'AUTH_SALT',        getenv('AUTH_SALT') );
+define( 'SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT') );
+define( 'LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT') );
+define( 'NONCE_SALT',       getenv('NONCE_SALT') );
+
+// Absolute path to the WordPress directory
+if ( !defined('ABSPATH') )
+    define('ABSPATH', dirname(__FILE__) . '/');
+
+// Sets up WordPress vars and included files
+require_once(ABSPATH . 'wp-settings.php');
